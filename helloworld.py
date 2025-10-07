23acff4dd3f5ba5b434745bc4fe12b8a69d8d898
rostomiii <rostom2823@gmail.com>
2025-10-07 17:42:54 +0000

    commit

--- helloworld.py
+++ helloworld.py
@@ -2,3 +2,14 @@ def hello():
     return 'Hello World!'
 
 print(hello())
+
+
+def sum_all(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return n + sum_all(n - 1)
+    
+print(sum_all(5))
